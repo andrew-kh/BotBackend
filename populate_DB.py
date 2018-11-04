d1487d7d1d8154b84ce433e17ecd833f6cdd7cf1
andrew-kh <andrey.khuhlin@gmail.com>
2018-11-04 21:05:43 +0300

    Add files via upload

--- populate_DB.py
+++ populate_DB.py
@@ -4,11 +4,14 @@ import numpy as np
 import pandas as pd
 from pymongo import MongoClient
 
-uri = "mongodb://<>:<>@ds215093.mlab.com:15093/medbot"
+# enter user/pwd wo <>
+uri = "mongodb://andrey.khuhlin:andrey.khuhlinABT1@ds215093.mlab.com:15093/medbot"
 client = MongoClient(uri)
-UL_folder = 'C:/Users/andrey/Documents/Mongo Backend/'
+UL_folder = '/Users/andrew/PycharmProjects/MedBot/'
 db = client.medbot
 
+# insert/append data from excel file to collection "message_dump"
+# this code will create this collection if it doesn't exist in DB
 messages = db.message_dump
 
 df_msgs = pd.read_excel(UL_folder + 'messages_UL.xlsx',
@@ -26,6 +29,8 @@ for row in range(0, df_msgs.shape[0]):
             dict_temp.update({dict_names[col]: df_msgs.iloc[row, col]})
     messages.insert_one(dict_temp)
 
+# insert/append data from excel file to collection "users"
+# this code will create this collection if it doesn't exist in DB
 users = db.users
 
 df_users = pd.read_excel(UL_folder + 'user_UL.xlsx',
@@ -39,10 +44,11 @@ for row in range(0, df_users.shape[0]):
         dict_temp.update({dict_names[col]: bson.int64.Int64(df_users.iloc[row, col])})
     users.insert_one(dict_temp)
 
+# add an array "services" to all users
 queries.add_inner_document(users, "services")
 
-queries.add_service_welcome_by_id(users, "welcome", 552)
+# assign an instance of "welcome" service to user with specific id
+queries.add_service_welcome_by_id(users, 552)
 
+# change status of service (when welcome message is sent)
 queries.chng_service_welcome_status_by_id(users, 552, 1)
-
-users.update({"uid": bson.int64.Int64(552)}, {"$pull": {"services": "welcome"}})
--- queries.py
+++ queries.py
@@ -15,7 +15,7 @@ def get_unix_now():
 
 
 def add_inner_document(obj_mongo_coll, str_coll_name):
-    """Add empty services collection to all users.\n
+    """Add empty services array to all users.\n
     In case of new deployment.
 
     Parameters
@@ -35,32 +35,31 @@ def add_inner_document(obj_mongo_coll, str_coll_name):
     return 1
 
 
-def add_service_welcome_by_id(obj_mongo_coll, str_serv_name, int_uid):
-    """Add empty services collection to all users.\n
-    In case of new deployment.
+def add_service_welcome_by_id(obj_mongo_coll, int_uid):
+    """Add an instance of "welcome" service to a specific user
 
     Parameters
     ----------
     obj_mongo_coll : MongoDB collection object
         Mongo collection for which to add empty collection
-    str_serv_name : string
-        name of service to insert
     int_uid : int
         user id to add service to
     """
+
+    str_serv_name = "welcome"
 
     int_uid = bson.int64.Int64(int_uid)
 
     unix_now = get_unix_now()
 
-    welcome_dict = {"welcome_msg": {"created": unix_now,
+    welcome_dict = {str_serv_name: {"created": unix_now,
                                     "status": 0}}
 
     obj_mongo_coll.update({'uid': int_uid}, {"$push": {"services": welcome_dict}})
 
 
 def chng_service_welcome_status_by_id(obj_mongo_coll, int_uid, int_val):
-    """Replaces welcome message service instance for a specific user
+    """Replaces welcome message service status for a specific user
 
     Parameters
     ----------
@@ -74,10 +73,12 @@ def chng_service_welcome_status_by_id(obj_mongo_coll, int_uid, int_val):
 
     int_uid = bson.int64.Int64(int_uid)
 
+    obj_mongo_coll.update_one({"uid": int_uid}, {"$pull": {"services": {"welcome.status": {"$exists": True}}}})
+
     unix_now = get_unix_now()
 
-    welcome_dict = {"welcome_msg": {"created": unix_now,
-                                    "status": int_val}}
+    welcome_dict = {"welcome": {"created": unix_now,
+                                "status": int_val}}
 
     obj_mongo_coll.update({"uid": int_uid},
                           {"$pull": {"services": "welcome"}})
